b3bc6010787e51e4d570cf2f87d1a7a02dba3aca
326th <minecraftskinman@gmail.com>
2021-02-28 10:07:33 +0700

    added damage system and animation

---
+++ Assets/Character Prefab/Test character assets/Script/DealDamage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealDamage: MonoBehaviour
+{
+
+    [SerializeField] float Damage = 5f;
+    [SerializeField] float Hurt_force = 10f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 11)
+        {
+            Test test = collision.GetComponentInParent<Test>();
+            test.Damage(Damage);
+        }
+    }
+}
--- Assets/Character Prefab/Test character assets/Script/Test.cs
+++ Assets/Character Prefab/Test character assets/Script/Test.cs
@@ -4,53 +4,73 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    //Finite States Machine
+    private enum State { Idle, Walk, Jump, Go_Up, Go_Down, Normal_Attack } // all states
+    private State state = State.Idle; // starting state
     //PLayer Components
     private Animator anim;
     private Rigidbody2D rb;
-    private Collider2D collider2D;
+    private Collider2D col;
     //Inspector variable
-    private float padding = 0.1f;
     [SerializeField] private LayerMask ground;
     [SerializeField] private float SPEED = 5f;
     [SerializeField] private float JUMP_VEL = 10f;
-    [SerializeField] private float hurtForce = 10f;
-    [SerializeField] private bool Facing_right = true;
+    [SerializeField] private bool busy = false; //during busy state (hit lag or hit stunt), player cannot move. this state is freed by animator
+    //Game logic
+    private float padding = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        collider2D = GetComponent<Collider2D>();
-        if (!Facing_right)
-        {
-            rb.transform.localScale = new Vector2(-1, 1);
-        }
+        col = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        AnimationState();
+        anim.SetInteger("State", (int)state);
     }
 
+    public void Damage(float damage)
+    {
+        print(damage);
+    }
+
+    private void AnimationState()
+    {
+        if (Mathf.Abs(rb.velocity.y) >= 0.1f)
+        {
+            state = State.Go_Up;
+        }
+        else
+        {
+            state = State.Idle;
+        }
+    }
     private void Movement()
     {
-        float hdirection = Input.GetAxis("Horizontal");
         // go left
-        if (hdirection < 0)
+        if (Input.GetKey("left")) //using left right to make the character stop immediately, horizontal axis > 0.9 can be consider
         {
             rb.velocity = new Vector2(-1 * SPEED, rb.velocity.y);
         }
         // go right
-        else if (hdirection > 0)
+        else if (Input.GetKey("right"))
         {
             rb.velocity = new Vector2(SPEED, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y); // to stop character on release
         }
         // jump
         if (Input.GetButtonDown("Jump"))
         {
-            RaycastHit2D hit = Physics2D.Raycast(collider2D.bounds.center, Vector2.down, collider2D.bounds.extents.y + padding, ground);
+            RaycastHit2D hit = Physics2D.Raycast(col.bounds.center, Vector2.down, col.bounds.extents.y + padding, ground); // check if there is ground beneath
             if (hit.collider != null) {
                 rb.velocity = new Vector2(rb.velocity.x, JUMP_VEL);
             }
